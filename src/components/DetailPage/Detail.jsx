a7f22c2d8cd5d35ffc3b1f52160cd869e9667568
Om Nigam <om2021064@akgec.ac.in>
2024-02-12 13:39:15 +0530

    fixing bugs

--- src/components/DetailPage/Detail.jsx
+++ src/components/DetailPage/Detail.jsx
@@ -13,10 +13,11 @@ const Detail = () => {
   const [tempInfo, setTempInfo] = useState({});
   const [weatherState, setWeatherState] = useState("");
   const [moreInfo, setMoreInfo] = useState(false);
+  const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API;
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${params.city}&units=metric&appid=94749519e1ef7e1c176399f75ffb04c3`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${params.city}&units=metric&appid=${API_KEY}`;
       let res = await fetch(url);
       let data = await res.json();
       const { temp, humidity, pressure } = data.main;
